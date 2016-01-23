7ef7f18c2cb54d3af39bb501ebc5d7d8d4819839
MrMendonez <dmendonez@gmail.com>
2016-01-23 09:12:41 -0500

    started main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+var prompt = require('prompt');
+var Word = require('./word');
+
+prompt.start();
+
+var game = {
+  wordBank: ["BLACK HAWK DOWN", "SHAUN OF THE DEAD", "ZOMBIELAND", "SAFETY NOT GUARANTEED", "SHERLOCK HOLMES", "UNSTOPPABLE", "FIGHT CLUB"];
+  guessRemaining: 10;
+  currentWrd: null;
+  startGame: function(wrd) {
+
+  }
+}
