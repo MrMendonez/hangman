7429cfa5ce9f1ec100bd46821ef9c3a36d224594
MrMendonez <dmendonez@gmail.com>
2016-01-22 23:31:48 -0500

    started working on checkIfLetterFound function. Have a question about the instructions. Need to verify with Pavan before moving on.

--- word.js
+++ word.js
@@ -5,8 +5,19 @@ function Word(wrd) {
   this.lets = [];
   this.found = false;
   this.getLets = function() {
-    for (var i = 0; i < word.length; i++) {
-      
+    for(var i = 0; i < word.length; i++) {
+      word.split(' ');
+      this.lets = Letter(this.word[i]);
+      console.log("lets = " + this.lets);
+    }
+  } // End getLets()
+  this.checkIfLetterFound = function(guessLetter) {
+    this.whatToReturn = 0;
+    for(var i = 0; i < lets.length; i++) {
+      if(Letter.charac === guessLetter) {
+        Letter.appear = true;
+        // HW says to add 1 to checkIfLetterFound. I'm thinking it should say 'add 1 to whatToReturn' since checkIfLetterFound is a function and whatToReturn is an integer. Need to verify.
+      }
     }
   }
 };
